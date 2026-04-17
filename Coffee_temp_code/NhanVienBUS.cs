b1eaeb945a30f4f0499f70c45fc1a5c6a7f4c747
Ng Lam Tan Loc <0306241302@caothang.edu.vn>
2026-04-17 08:23:54 +0700

    Add files via upload

---
+++ Coffee_temp_code/NhanVienBUS.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLQuanCaPheBUS
+{
+    public class NhanVienBUS
+    {
+        public NhanVienDTO DangNhap(string username, string password)
+        {
+            NhanVienDAO nvDAO = new NhanVienDAO();
+            return nvDAO.DangNhap(username, password);
+        }
+    }
+}
---
+++ Coffee_temp_code/SanPhamBUS.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLQuanCaPheBUS
+{
+    public class SanPhamBUS
+    {
+        public SanPhamDTO DangNhap(string username, string password)
+        {
+            
+        }
+    }
+}
